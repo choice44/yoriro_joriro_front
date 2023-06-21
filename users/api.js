31d924ba60dddf579eb6e65022b9ce4c5ddcf43d
Choi Seungwon <cso6549@naver.com>
2023-06-21 16:44:14 +0900

    [수정] 내비바 프로필 버튼 로그인 유무에 따라 변화

--- users/api.js
+++ users/api.js
@@ -1,4 +1,18 @@
-async function handleLogout() {
+document.addEventListener('DOMContentLoaded', function () {
+
+  const profile = document.querySelector('#profile')
+
+  if (localStorage.getItem("payload")) {
+
+    profile.innerHTML = `
+        <li><a href="/index.html">마이페이지</a></li>
+        <li><a onclick="handleLogout()">로그아웃</a></li>
+      `
+  }
+})
+
+
+function handleLogout() {
   localStorage.removeItem("access");
   localStorage.removeItem("refresh");
   localStorage.removeItem("payload");
