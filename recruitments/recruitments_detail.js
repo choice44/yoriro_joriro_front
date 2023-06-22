666b896e2dac5d7c895c8c99c2609de4992097d7
Lee DoYoon <ehdbs1276@naver.com>
2023-06-22 09:36:23 +0900

    [추가][수정]출력페이지 수정
    [수정]출력될때 조금더 이쁩니다.
    [추가]css파일을 추가하였습니다.
    
    [추가]recruitments/recruitments.css
    - 이미지 섬네일에 필요한 css와 페이지네이션 css 입니다.
    
    [수정]recruitments/recruitments.html 및 html파일들
    - 생김새를 수정하였습니다.
    
    [수정]recruitments/recruitments.js 및 js파일들
    - html이 변경됨에 따라 js도 바뀌고, innerHTML을 사용하는 등으로
      수정하였습니다.

--- recruitments/recruitments_detail.js
+++ recruitments/recruitments_detail.js
@@ -1,9 +1,10 @@
+// import { proxy } from "../proxy.js";
 const proxy = "http://127.0.0.1:8000"
 // const proxy = "https://api.bechol.com"
 const front_proxy = "http://127.0.0.1:5500"
 
 let recruitmentId
-// let applicantId
+let applicantId
 
 window.onload = async function () {
     const urlParams = new URLSearchParams(window.location.search)
@@ -12,28 +13,64 @@ window.onload = async function () {
 
     await loadRecruitmentDetail(recruitmentId)
     await loadJoin(recruitmentId)
-    await loadParticipant(recruitmentId)
     console.log("loadJoin에 들어가는 recruitmentId", recruitmentId)
 }
 
 
 async function loadRecruitmentDetail(recruitmentId) {
     const response = await getRecruitment(recruitmentId)
-    const recruitmentReset = document.getElementById("recruitment")
-    recruitmentReset.value = ""
 
-    const recruitmentButtons = document.getElementById("recruitment-buttons")
-    if (await checkAuthor(recruitmentId) == true) {
-        recruitmentButtons.style.display = "block"
+    if (response.image) {
+        imagePath = proxy + response.image;
     } else {
-        recruitmentButtons.style.display = "none"
+        imagePath = "/images/car-2.jpg"
     }
+    const recruitmentImage = document.getElementById("recruitment-image")
+    recruitmentImage.innerHTML = `
+    <img src="${imagePath}" height="300px" style="object-fit: cover; object-position: center; width: 100%; margin-top:50px;">
+    `
+    console.log(response)
+    console.log(imagePath)
 
-    const recruitmentWriter = document.getElementById("writer-info")
-    recruitmentWriter.innerText = response.user
-    console.log("userinfo : ", response.user)
-    nickname = response.user.nickname
-    age = response.user.age
+    const recruitmentTitle = document.getElementById("recruitment-title")
+    recruitmentTitle.setAttribute("style", "margin-top:50px; margin-bottom:30px")
+    recruitmentTitle.innerText = `${response.title}`
+
+    const recruitmentLoad = document.getElementById("recruitment")
+    recruitmentLoad.value = ""
+
+    const template = document.createElement("div")
+    template.innerHTML = ""
+    departure = response.departure.split('T')[0]
+    arrival = response.arrival.split('T')[0]
+    updated_at = response.updated_at.split('T')[0] + " " + response.updated_at.split('T')[1].split('.')[0]
+    template.innerHTML = `
+        <table width="100%" style="text-align:center; margin-right:5%; margin-top:5%; border: 1px solid #444444; border-collapse:separate; border-radius:8px;">
+            <tr height=60px>
+                <td style="text-align:right">장소</td>
+                <th style="text-align:center">${response.place}</th>
+                <td style="text-align:right">경비</td>
+                <th style="text-align:center">${response.cost}원</th>
+                <td style="text-align:right">모집 정원</td>
+                <th style="text-align:center">${response.participant.length + "/" + response.participant_max}</th>
+                <td width=20px></td>
+            </tr>
+            <tr height=70px>
+                <td style="text-align:right">기간</td>
+                <th style="text-align:center" colspan="5">${departure + ' ~ ' + arrival}</th>
+            </tr>
+        </table>
+        
+        <table width="100%" style="text-align:center; margin-right:5%; margin-top:10%; margin-bottom:10%">
+            <tr>
+                <td>${response.content}</td>
+            </tr>
+        </table>
+
+        <div>updated : ${updated_at}
+        </div>`
+    recruitmentLoad.appendChild(template)
+
     gender = response.user.gender
     if (gender == "F") {
         gender = "여성"
@@ -43,80 +80,77 @@ async function loadRecruitmentDetail(recruitmentId) {
         gender = "설정하지 않음"
     }
 
-    recruitmentWriter.innerText = nickname + " / " + age + " / " + gender
+    const recruitmentWriterInfo = document.getElementById("writer-info")
+    recruitmentWriterInfo.innerHTML = `
+        <table width="80%" style="margin-top:40%;margin-left:25%;">
+            <tr>
+                <th colspan="2" style="text-align:center;">${response.user.nickname}</th>
+            </tr>
+            <tr>
+                <td width="30px" style="text-align:center;">나이</td>
+                <th width="30px" style="text-align:center;">${response.user.age}</th>
+            </tr>
+            <tr>
+                <td style="text-align:center;">성별</td>
+                <th style="text-align:center;">${gender}</th>
+            </tr>
+        </table>`
 
-    const recruitmentTitle = document.getElementById("recruitment-title")
-    const recruitmentPlace = document.getElementById("recruitment-place")
-    const recruitmentImage = document.getElementById("recruitment-image")
-    recruitmentImage.innerHTML = ""
-    const recruitmentContent = document.getElementById("recruitment-content")
-    const recruitmentPeriod = document.getElementById("recruitment-period")
-    const recruitmentCost = document.getElementById("recruitment-cost")
-    const recruitmentParticipant = document.getElementById("recruitment-participant")
-
-    recruitmentTitle.innerText = response.title
-    recruitmentPlace.innerText = response.place
-
-    const newImage = document.createElement("img")
-
-    if (response.image != null) {
-        newImage.setAttribute("src", `${proxy}${response.image}`)
-    } else {
-        newImage.setAttribute("src", `../images/car-2.jpg`)
-    }
-
-    newImage.setAttribute("class", "col-sm-12 col-md-12")
-    console.log(response.image)
-    recruitmentImage.appendChild(newImage)
-
-    recruitmentContent.innerText = response.content
-
-    departure = response.departure.split('T')[0]
-    arrival = response.arrival.split('T')[0]
-    recruitmentPeriod.innerText = departure + " ~ " + arrival
-
-    recruitmentCost.innerText = response.cost + "원"
-    recruitmentParticipant.innerText = response.participant.length + "/" + response.participant_max
-}
-
-
-async function loadParticipant(recruitmentId) {
-    const response = await getRecruitment(recruitmentId)
-
-    console.log("test", response.participant)
     participant = response.participant
-    const participantCount = document.getElementById("participant-count")
-    participantCount.innerText = ""
-    const participantList = document.getElementById("participant-list")
-    participantList.innerText = ""
+    console.log("participant", participant)
 
-    participantCount.innerText = "참가자 수 : " + participant.length
-
+    const participantCard = document.getElementById("participant")
+    const participantTable = document.createElement("table")
+    participantTable.innerHTML = ""
+    // participantTable.setAttribute("border", "1")
+    participantTable.setAttribute("width", "80%")
+    participantTable.setAttribute("style", "margin-top:20%;margin-left:25%;color:black; border-collapse:separate; border-radius:30px; border: 1px solid #444444; ")
+    participantTable.innerHTML = `
+        <tr><td height="5px"></td>
+        </tr>
+        <tr>
+            <th colspan="3" style="text-align:center;">참가자 : ${participant.length}명</th>
+        </tr>
+    `
     participant.forEach(user => {
-        const participantNickname = document.createElement("div")
-        participantNickname.innerText = user.nickname
+        if (user.gender == "F") {
+            user.gender = "여성"
+        } else if (user.gender == "M") {
+            user.gender = "남성"
+        } else {
+            user.gender = "지정하지 않음"
+        }
 
-        const participantAge = document.createElement("div")
-        participantAge.innerText = user.age
-
-        const pariticipantGender = document.createElement("div")
-        pariticipantGender.innerText = user.gender
-
-        participantList.appendChild(participantNickname)
-        participantList.appendChild(participantAge)
-        participantList.appendChild(pariticipantGender)
+        participantTable.innerHTML += `
+        <tr>
+            <th height="30px" rowspan="2" style="text-align:center;">${user.nickname}</th>
+            <td style="text-align:center;">${user.age}세</td>
+            <td style="text-align:center;">${user.gender}</td>
+        </tr>
+        `
     })
+
+    participantTable.innerHTML += `
+    <tr><td height="10px"></td>
+    </tr>    
+    `
+    participantCard.appendChild(participantTable)
+
+    const result = await checkAuthor(recruitmentId);
+    if (result) {
+        const recruitmentButton = document.getElementById("recruitment-button")
+        recruitmentButton.style.display = "block"
+    }
 }
 
 
 async function getRecruitment(recruitmentId) {
-    // const response = await fetch(`${proxy}/recruitments/${recruitmentId}/`)
-    const token = localStorage.getItem('access');
+    const token = localStorage.getItem('access')
     const response = await fetch(`${proxy}/recruitments/${recruitmentId}/`, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
-    });
+    })
 
     if (response.status == 200) {
         response_json = await response.json()
@@ -128,13 +162,12 @@ async function getRecruitment(recruitmentId) {
 
 
 async function getApplicant(recruitmentId) {
-    // const response = await fetch(`${proxy}/recruitments/${recruitmentId}/join`)
-    const token = localStorage.getItem('access');
+    const token = localStorage.getItem('access')
     const response = await fetch(`${proxy}/recruitments/${recruitmentId}/join`, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
-    });
+    })
 
     if (response.status == 200) {
         const response_json = await response.json()
@@ -146,7 +179,10 @@ async function getApplicant(recruitmentId) {
 
 
 async function loadJoin(recruitmentId) {
-    const applicantResponse = await getApplicant(recruitmentId)
+    const applicantResponse = await getApplicant(recruitmentId);
+    const accessToken = localStorage.getItem('access')
+    userId = getPKFromAccessToken(accessToken)
+
     const result = await checkAuthor(recruitmentId)
     const joinReset = document.getElementById("applicant")
     joinReset.value = ""
@@ -162,86 +198,56 @@ async function loadJoin(recruitmentId) {
     const applicantList = document.getElementById("applicant-list")
     applicantList.innerHTML = ""
 
-    applicantCount.innerText = "신청자 수 : " + applicantResponse.length
+    // applicantCount.innerText = "신청자 수 : " + applicantResponse.length;
 
-    applicantResponse.forEach(async applicant => {
-        nickname = applicant.user.nickname
-        age = applicant.user.age
-        gender = applicant.user.gender
-        appeal = applicant.appeal
-        acceptence = applicant.acceptence
-        applicantId = applicant.id
+    applicantResponse.forEach(applicant => {
+        const { user, appeal, acceptence, id } = applicant
+        const { nickname, age, gender } = user
 
-        const applicantNickname = document.createElement("div")
-        applicantNickname.innerText = nickname
+        genderPrint = ""
+        acceptencePrint = ""
 
-        const applicantAppeal = document.createElement("div")
-        applicantAppeal.setAttribute('id', `appeal-${applicantId}`)
-        applicantAppeal.innerText = appeal
-
-        const applicantAge = document.createElement("div")
-        applicantAge.innerText = age
-
-        const applicantGender = document.createElement("div")
-        applicantGender.innerText = gender
-
-        const applicantAcceptence = document.createElement("div")
-        applicantAcceptence.innerText = acceptence
-
-        if (acceptence == 0) {
-            applicantAcceptence.innerText = "대기중"
-        } else if (acceptence == 1) {
-            applicantAcceptence.innerText = "거절"
+        if (gender == "M") {
+            genderPrint = "남성"
+        } else if (gender == "F") {
+            genderPrint = "여성"
         } else {
-            applicantAcceptence.innerText = "수락"
+            genderPrint = ""
         }
-
-        applicantList.appendChild(applicantNickname)
-        applicantList.appendChild(applicantAppeal)
-
-        if (applicant.user.id == userId) {
-            const editButton = document.createElement("button")
-            editButton.setAttribute("id", 'edit-appeal-button')
-            editButton.setAttribute("onclick", `editJoin(${applicantId})`)
-            editButton.innerText = "수정"
-            applicantList.appendChild(editButton)
-
-            const deleteButton = document.createElement("button")
-            deleteButton.setAttribute("id", 'delete-appeal-button')
-            deleteButton.setAttribute("onclick", `deleteJoin(${applicantId})`)
-            deleteButton.innerText = "삭제"
-            applicantList.appendChild(deleteButton)
-        }
-
-        applicantList.appendChild(applicantAge)
-        applicantList.appendChild(applicantGender)
-        applicantList.appendChild(applicantAcceptence)
-
-        const accessToken = localStorage.getItem('access')
-        userId = getPKFromAccessToken(accessToken)
-        console.log("userId", userId)
-        console.log("recruitment.user", applicant.user.id)
 
         if (acceptence == 0) {
-            if (result == true) {
-                const applicantButton = document.createElement("div")
-                const accept = document.createElement("button")
-                accept.setAttribute("onclick", `acceptApplicant(${applicant.id})`)
-                accept.innerText = "수락"
-                const reject = document.createElement("button")
-                reject.setAttribute("onclick", `rejectApplicant(${applicant.id})`)
-                reject.innerText = "거절"
-                applicantButton.appendChild(accept)
-                applicantButton.appendChild(reject)
-                applicantList.appendChild(applicantButton)
-            }
+            acceptencePrint = "대기중"
+        } else if (acceptence == 1) {
+            acceptencePrint = "거절됨"
+        } else {
+            acceptencePrint = "수락됨"
         }
-    })
+
+        const tableHTML = `
+        <table class="col-md-12">
+            <tr>
+                <th>${nickname}</th>
+                <td width="4%" style="text-align:right">${age}</td>
+                <td width="8%" style="text-align:right">${genderPrint}</td>
+                <td width="80"></td>
+            </tr>
+        </table>
+        <div style="margin-bottom:5%">
+            <div width="100%" colspan="4" id="appeal-${id}">${appeal}</div>
+            <div style="display: flex; flex-direction: row;"">
+                <input type="button" value="수정" id="edit-appeal-button-${id}" onclick="editJoin(${id})" style="margin-bottom: 5px; display: ${user.id == userId ? 'block' : 'none'}">
+                <input type="button" value="수락" id="accept-appeal-button" onclick="acceptApplicant(${id})" style="margin-bottom: 5px; display: ${acceptence == 0 && result ? 'block' : 'none'}">
+                <input type="button" value="삭제" id="delete-appeal-button" onclick="deleteJoin(${id})" style="margin-bottom: 5px; display: ${user.id == userId ? 'block' : 'none'}">
+                <input type="button" value="거절" id="reject-appeal-button" onclick="rejectApplicant(${id})" style="margin-bottom: 5px; display: ${acceptence == 0 && result ? 'block' : 'none'}">
+            </div>
+        </div>`
+
+        applicantList.innerHTML += tableHTML
+    });
 }
 
 
 async function getJoin(recruitmentId) {
-    // const response = await fetch(`${proxy}/recruitments/${recruitmentId}/join/`)
     const token = localStorage.getItem('access');
     const response = await fetch(`${proxy}/recruitments/${recruitmentId}/join/`, {
         headers: {
@@ -397,8 +403,9 @@ async function rejectApplicant(applicantId) {
 
 async function editJoin(applicantId) {
     let token = localStorage.getItem("access")
-    const hideEditButton = document.getElementById("edit-appeal-button")
-    hideEditButton.style.display = "none"
+    const hideEditButton = document.getElementById(`edit-appeal-button-${applicantId}`)
+    // hideEditButton.style.display = "none"
+    // hideEditButton.setAttribute("style", "display:none")
 
     const joinCard = document.getElementById(`appeal-${applicantId}`)
     const textBox = document.createElement("input")
@@ -407,8 +414,10 @@ async function editJoin(applicantId) {
     joinCard.innerHTML = ""
     joinCard.appendChild(textBox)
 
-    const saveButton = document.createElement("button")
-    saveButton.innerText = "저장"
+    const saveButton = document.createElement("input")
+    saveButton.setAttribute("type", "button")
+    saveButton.setAttribute("value", "저장")
+    saveButton.setAttribute("class", "btn btn-primary")
     saveButton.addEventListener("click", async () => {
         const updatedAppeal = textBox.value
         joinCard.innerHTML = updatedAppeal
@@ -446,7 +455,6 @@ async function deleteJoin(applicantId) {
             loadRecruitmentDetail(recruitmentId)
             loadJoin(recruitmentId)
             loadParticipant(recruitmentId)
-            // location.replace(`recruitments_detail.html??recruitment_id=${recruitment_id}`)
         } else {
             alert("권한이 없습니다.")
         }
