6799df117ddf599f2fe0f8cedd81884f79bd3005
Lee DoYoon <ehdbs1276@naver.com>
2023-06-25 18:44:33 +0900

    [수정]페이지네이션 버튼을 더보기 버튼으로 수정
    [수정]페이지네이션을 대신하여 더보기버튼을 추가하였습니다.

--- recruitments/recruitments.js
+++ recruitments/recruitments.js
@@ -10,7 +10,7 @@ window.onload = async function () {
     let recruitments = await getRecruitments(currentPage)
 
     loadRecruitments(recruitments)
-    pagination(recruitments, parseInt(currentPage))
+    // pagination(recruitments, parseInt(currentPage))
 }
 
 
@@ -21,7 +21,6 @@ async function loadRecruitments(recruitments) {
         const template = document.createElement("div");
         template.setAttribute("class", "col-md-4 col-sm-6 fh5co-tours animate-box fadeInUp animated");
         template.setAttribute("data-animate-effect", "fadeIn");
-        template.setAttribute("onclick", `recruitmentDetail(${recruitment.id})`)
 
         let imagePath = ""
 
@@ -50,11 +49,23 @@ async function loadRecruitments(recruitments) {
                 <h3 style="color:${recruitment.is_complete == 0 ? 'red' : 'gray'};">${status[recruitment.is_complete]}</h3>
                 <span style="color:${recruitment.participant.length == recruitment.participant_max || recruitment.is_complete != 0 ? 'gray;">모집 완료 ' : 'white;">모집 현황 '}
                 <h3 style="display:inline; color:${recruitment.participant.length == recruitment.participant_max || recruitment.is_complete != 0 ? 'gray;"' : 'white;"'}>${recruitment.participant.length + '/' + recruitment.participant_max}</h3></span>
-                <a class="btn btn-primary btn-outline" href="#">지원하기 <i class="icon-arrow-right22"></i></a>
+                <a class="btn btn-primary btn-outline" href="/recruitments/detail/index.html?recruitment_id=${recruitment.id}">지원하기 <i class="icon-arrow-right22"></i></a>
             </div>
         </div>`
         recruitment_list.appendChild(template)
     })
+
+    if (recruitments.next) {
+        const template = document.createElement("div");
+
+        template.setAttribute("id", "recruitment_more_button");
+        template.setAttribute("class", "col-md-12 text-center");
+
+        template.innerHTML = `<input type="button" class="btn btn-primary btn-lg" onclick="viewMoreRecruitments('${proxy}/${recruitments.next}')"
+            value="▼ 더보기 ▼" />`;
+
+        recruitment_list.appendChild(template)
+    }
 }
 
 
@@ -89,53 +100,73 @@ async function getRecruitments(page) {
 }
 
 
-// 페이지네이션
-async function pagination(recruitments, currentPage) {
-    const pageButton = document.getElementById("pagination")
+async function viewMoreRecruitments(nextURL) {
+    try {
+        // 데이터 요청함수를 이용하여 다음 페이지의 데이터를 불러옵니다.
+        const response = await fetch(nextURL);
+        const recruitments_all = await response.json();
+        const recruitments = recruitments_all.results;
+        const recruitmentsList = document.getElementById("recruitment-list");
 
-    const previousButton = document.createElement("a")
-    previousButton.setAttribute("class", "btn btn-primary btn-outline")
-    previousButton.innerText = "이전 페이지"
+        // for문 돌면서 게시글들을 생성
+        recruitments.forEach((recruitment) => {
+            const template = document.createElement("div");
+            template.setAttribute("class", "col-md-4 col-sm-6 fh5co-tours animate-box fadeInUp animated");
+            template.setAttribute("data-animate-effect", "fadeIn");
 
-    // 클릭이 되면 이전 페이지로 이동
-    previousButton.addEventListener("click", function () {
-        const prevPage = currentPage - 1
-        window.location.href = `index.html?page=${prevPage}`
-    });
+            let imagePath = "/images/car-2.jpg"
 
-    // 이전 페이지가 없으면 버튼 보이지 않게 설정
-    if (recruitments.previous == null) {
-        previousButton.style.display = "none"
+            // 백엔드 주소 같이 출력되는 것을 제거
+            if (recruitment.image) {
+                imagePath = proxy + '/' + recruitment.image;
+            }
+
+            const status = { 0: "모집 중", 1: "모집 완료", 2: "여행 중", 3: "여행 완료" };
+            let departure = recruitment.departure.split('T')[0]
+            let arrival = recruitment.arrival.split('T')[0]
+
+            template.innerHTML = `
+            <div><img src="${imagePath}" alt="동료 모집 게시글 이미지" class="img-responsive recruitment-image-thumbnail">
+                <div class="desc">
+                    <span></span>
+                    <h3>${recruitment.title}</h3>
+                    <span>${recruitment.place}</span>
+                    <span>${departure + ' ~ ' + arrival}</span>
+                    <h3 style="color:${recruitment.is_complete == 0 ? 'red' : 'gray'};">${status[recruitment.is_complete]}</h3>
+                    <span style="color:${recruitment.participant.length == recruitment.participant_max || recruitment.is_complete != 0 ? 'gray;">모집 완료 ' : 'white;">모집 현황 '}
+                    <h3 style="display:inline; color:${recruitment.participant.length == recruitment.participant_max || recruitment.is_complete != 0 ? 'gray;"' : 'white;"'}>${recruitment.participant.length + '/' + recruitment.participant_max}</h3></span>
+                    <a class="btn btn-primary btn-outline" href="/recruitments/detail/index.html?recruitment_id=${recruitment.id}">지원하기 <i class="icon-arrow-right22"></i></a>
+                </div>
+            </div>`
+
+            recruitmentsList.appendChild(template);
+        })
+
+        // 기존의 '더보기' 버튼을 제거
+        const oldButton = document.getElementById("recruitment_more_button");
+        if (oldButton) {
+            oldButton.remove();
+        }
+
+        // 만약 다음 페이지가 있으면, 새로운 '더보기' 버튼을 생성합니다.
+        if (recruitments_all.next) {
+            const template = document.createElement("div");
+
+            template.setAttribute("id", "recruitment_more_button");
+            template.setAttribute("class", "col-md-12 text-center");
+
+            template.innerHTML = `<input type="button" class="btn btn-primary btn-lg" onclick="viewMoreRecruitments('${proxy}/${recruitments_all.next}')"
+                    value="▼ 더보기 ▼" />`;
+
+            recruitmentsList.appendChild(template);
+        }
+
+    } catch (error) {
+        console.log("에러가 발생했습니다", error);
     }
-
-    const nextButton = document.createElement("a")
-    nextButton.setAttribute("class", "btn btn-primary btn-outline")
-    nextButton.innerText = "다음 페이지"
-
-    // 클릭이 되면 다음 페이지로 이동
-    nextButton.addEventListener("click", function () {
-        const nextPage = currentPage + 1
-        window.location.href = `index.html?page=${nextPage}`
-    });
-
-    // 다음페이지가 없으면 버튼 보이지 않게 설정
-    if (recruitments.next == null) {
-        nextButton.style.display = "none"
-    }
-
-    // 버튼 가운데에 현재 페이지를 보여주는 버튼 추가
-    // 클릭 불가능
-    const currentPageShow = document.createElement("button")
-    currentPageShow.setAttribute("id", "current-page")
-    currentPageShow.setAttribute("class", "btn")
-    currentPageShow.innerText = currentPage
-    currentPageShow.disabled = true;
-
-    pageButton.appendChild(previousButton)
-    pageButton.appendChild(currentPageShow)
-    pageButton.appendChild(nextButton)
 }
 
 
 window.recruitmentDetail = recruitmentDetail
 window.CreateRecruitment = CreateRecruitment
+window.viewMoreRecruitments = viewMoreRecruitments
