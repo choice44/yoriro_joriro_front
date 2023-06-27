74e351b5eaa8de4786456629335acb172e2cbc1f
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-06-27 19:49:34 +0900

    [수정]소셜로그인만 가능하도록 수정

--- users/login/login.js
+++ users/login/login.js
@@ -72,5 +72,5 @@ function checkLogin() {
     }
 }
 
-const login = document.getElementById('login-btn');
-login.addEventListener('click', handleLogin);
+// const login = document.getElementById('login-btn');
+// login.addEventListener('click', handleLogin);
