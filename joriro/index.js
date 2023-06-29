32a4282d8bf9548147357c4b1346c9de4cf5ab21
Choi Seungwon <cso6549@naver.com>
2023-06-29 10:35:24 +0900

    [수정] 조리로 페이지 설명 추가, 결과 사진 디자인 수정

--- joriro/index.js
+++ joriro/index.js
@@ -109,6 +109,9 @@ function appendResult(data) {
     showBox.innerHTML = `
 
     <img src=${imageSrc} alt="로드 실패" class="img-fluid"></img>
+    <div class="down_btn">
+      <a href=${imageSrc} download><button>이미지 원본 다운로드</button></a>
+    </div>
     `
 
 }
